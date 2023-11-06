482631abda347150d0b92bf40f547d700c24db77
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2023-11-06 01:19:20 -0400

    Add Event base class

---
+++ pyeelib/events/event.py
@@ -0,0 +1,42 @@
+from abc import ABC
+from dataclasses import dataclass, field
+from datetime import datetime
+
+
+@dataclass(frozen=True)
+class Event(ABC):
+    """
+    An abstract base class for event objects.
+
+    This class serves as a blueprint for creating event objects.
+    It is intended to be subclassed and extended by concrete event classes.
+
+    Example:
+    ```py
+    @dataclass(frozen=True)
+    class OrderCreated(Event):
+        id: str
+
+    event = OrderCreated(id="65486c788bba30ff7e0152ae")
+    ```
+    """
+
+    timestamp: datetime = field(init=False, default_factory=datetime.now)
+    """ The date and time when the event occurred. """
+
+    @property
+    def name(self) -> str:
+        """
+        Gets the name of the event.
+        :return: The name of the event as a string.
+        """
+        return self.__class__.__name__
+
+    def __str__(self) -> str:
+        """
+        Returns a formatted string representation of the event.
+        :return: The formatted string representation of the event.
+        """
+        default, highlight = '\033[0m', '\033[32m'
+        return (f"{highlight}{self.name}{default}  {self.timestamp.strftime('%Y-%m-%d %H:%M:%S %p')}"
+                f"\t{highlight}[Payload]{default} {super().__str__()}")

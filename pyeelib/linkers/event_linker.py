4c4d2d7220267b8593786d57e415a60c3f4c9316
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2023-11-12 19:34:29 -0400

    Update EventLinker class with docstring and thread-safe support

--- pyeelib/linkers/event_linker.py
+++ pyeelib/linkers/event_linker.py
@@ -1,105 +1,304 @@
-from typing import Dict, List, Mapping, ParamSpec, Callable, Type, Coroutine
+from abc import ABC
+from itertools import chain
+from sys import version_info
+from threading import Lock
+from typing import Dict, List, Mapping, Callable, Type, TypeAlias, Any, Set
 
 from pyeelib.events import Event
+from pyeelib.exceptions import PyeelibException
 from pyeelib.listeners import EventListener
 
+if version_info >= (3, 10):  # pragma: no cover
+    from typing import ParamSpec
+else:  # pragma: no cover
+    from typing_extensions import ParamSpec
+
+SubscribableEventType: TypeAlias = Type[Event] | Type[BaseException] | Type[object] | str
+""" A type alias representing the supported event types for subscription. """
+
 P = ParamSpec("P")
+""" A generic type to represent the parameter names and types of the callback function. """
 
 
-class EventLinker:
-    _EVENT_TYPES = Type[Event] | Type[BaseException] | str
+class EventLinker(ABC):
+    """
+    A base class that acts as a global registry for linking events with their associated event listeners.
 
-    _event_listeners: Dict[str, List[EventListener]] = {}
+    The `EventLinker` class provides a centralized mechanism for managing event subscriptions, unsubscriptions,
+    and retrieval of events and their associated event listeners. It acts as a global registry, ensuring that
+    events and their listeners are organized and easily accessible.
+
+    **Note**: The `EventLinker` class can be subclassed to create specific namespaces or contexts for managing
+    events and event listeners. By subclassing `EventLinker`, users can separate and organize event subscriptions
+    and listeners within different scopes, providing modularity and flexibility in event management.
+    """
+
+    _event_registry: Dict[str, List[EventListener]] = {}
+    """ 
+    A dictionary that serves as a container for storing events and their respective event listeners.
+    The keys represent event names, and the values are lists of `EventListener` objects. 
+    """
+
+    _max_listeners: int | None = None
+    """ The maximum number of `EventListener` allowed per event, or None if no limit. """
+
+    _thread_lock: Lock = Lock()
+    """
+    A threading.Lock object used for thread synchronization when accessing and modifying the event registry
+    to ensure thread safety. It prevents multiple threads from accessing or modifying the registry simultaneously.
+    """
 
     @classmethod
     @property
     def events(cls) -> List[str]:
-        return list(cls._event_listeners.keys())
+        """
+        Returns a list of all the registered event names.
+        :return: A list of event names.
+        """
+        with cls._thread_lock:
+            return list(cls._event_registry.keys())
 
     @classmethod
     @property
-    def listeners(cls) -> List[EventListener]:
-        event_listeners: List[EventListener] = []
-        for event_listener in cls._event_listeners.values():
-            event_listeners.extend(event_listener)
-        return list(set(event_listeners))
+    def event_listeners(cls) -> List[EventListener]:
+        """
+        Retrieves a list of non-duplicated event listeners
+        who have been registered across all events.
+        :return: A list of event listeners.
+        """
+        with cls._thread_lock:
+            return list(set(chain(*cls._event_registry.values())))
 
     @classmethod
     @property
-    def event_listeners(cls) -> Mapping[str, List[EventListener]]:
-        return cls._event_listeners
+    def event_registry(cls) -> Mapping[str, List[EventListener]]:
+        """
+        Retrieves the event registry mapping.
+        :return: A mapping of event names to event listeners.
+        """
+        with cls._thread_lock:
+            return cls._event_registry
 
     @classmethod
-    def _event_key(cls, event: _EVENT_TYPES) -> str:
+    @property
+    def max_listeners(cls) -> None | int:
+        """
+        Retrieves the maximum number of listeners allowed per event.
+        :return: The maximum number of listeners or None if there is no limit.
+        """
+        return cls._max_listeners
+
+    @classmethod
+    def _get_event_key(cls, event: SubscribableEventType) -> str:
+        """
+        Determines the event string key based on the given event.
+        :param event: The event to obtain the key for.
+        :return: The event string key.
+        :raise PyeelibException: If the event type is not supported.
+        """
         if isinstance(event, str):
             return event
-        elif issubclass(event, (Event, BaseException)):
+        elif issubclass(event, (Event, BaseException, object)):
             return event.__name__
         else:
-            raise ValueError('Unsupported event type')
+            raise PyeelibException('Unsupported event type')
 
     @classmethod
     def get_events_by_listener(cls, event_listener: EventListener) -> List[str]:
-        return [k for k, v in cls._event_listeners.items() if event_listener in v]
+        """
+        Retrieves a list of event names associated with the specified event listener.
+        :param event_listener: The listener to retrieve the associated events for.
+        :return: A list of event names.
+        """
+        with cls._thread_lock:
+            return [
+                event
+                for event, event_listeners in cls._event_registry.items()
+                if event_listener in event_listeners
+            ]
 
     @classmethod
-    def get_listeners_by_event(cls, event: _EVENT_TYPES) -> List[EventListener]:
-        key: str = cls._event_key(event=event)
-        return cls._event_listeners.get(key, [])
+    def get_listeners_by_event(cls, event: SubscribableEventType) -> List[EventListener]:
+        """
+        Retrieves a list of event listeners associated with the specified event.
+        :param event: The event to retrieve the listeners for.
+        :return: A list of event listeners.
+        """
+        key: str = cls._get_event_key(event=event)
+        with cls._thread_lock:
+            return cls._event_registry.get(key, [])
 
     @classmethod
-    def on(cls, *events: _EVENT_TYPES, ttl: int | None = None):
+    def once(cls, *events: SubscribableEventType) -> Callable[[Callable[P, Any]], Callable[P, Any]]:
+        """
+        Decorator that subscribes a callback function to the specified events to be invoked only once.
+        This decorator is used to conveniently subscribe a callback function as a one-time listener.
+        :param events: The events to subscribe to.
+        :return: The decorator function that wraps the callback.
+        """
 
-        def _decorator(func: Callable[P, Coroutine | None]):
-            cls.subscribe(*events, callback=func, ttl=ttl)
-            return func
+        def _decorator(callback: Callable[P, Any]) -> Callable[P, Any]:
+            cls.subscribe(*events, callback=callback, once=True)
+            return callback
 
         return _decorator
 
     @classmethod
-    def subscribe(cls, *events: _EVENT_TYPES, callback: Callable[P, Coroutine | None], ttl: int | None = None) -> None:
-        event_listener: EventListener = EventListener(callback=callback, ttl=ttl)
+    def on(cls, *events: SubscribableEventType) -> Callable[[Callable[P, Any]], Callable[P, Any]]:
+        """
+        Decorator that subscribes a callback function to the specified events.
+        :param events: The events to subscribe to.
+        :return: The decorator function that wraps the callback.
+        """
 
-        for event in events:
-            key: str = cls._event_key(event=event)
+        def _decorator(callback: Callable[P, Any]) -> Callable[P, Any]:
+            cls.subscribe(*events, callback=callback, once=False)
+            return callback
 
-            if key not in cls._event_listeners:
-                cls._event_listeners[key] = []
-
-            cls._event_listeners[key].append(event_listener)
-
-    @classmethod
-    def unsubscribe(cls, *events: _EVENT_TYPES, event_listener: EventListener) -> None:
-        for event in events:
-            key: str = cls._event_key(event=event)
-
-            event_listeners = cls._event_listeners.get(key, [])
-
-            if event_listener in event_listeners:
-                event_listeners.remove(event_listener)
-
-                if not event_listeners:
-                    cls._event_listeners.pop(key)
+        return _decorator
 
     @classmethod
-    def remove_listener(cls, event_listener: EventListener):
-        for event, listeners in cls._event_listeners.items():
-            if event_listener in listeners:
-                listeners.remove(event_listener)
+    def subscribe(cls, *events: SubscribableEventType, callback: Callable[P, Any], once: bool = False) -> EventListener:
+        """
+        Subscribes a callback function to the specified events.
+        :param events: The events to subscribe to.
+        :param callback: The callback function to associate with the events.
+        :param once: Specifies if the callback function is a one-time listener. If set to `True`,
+            the listener will be invoked once when the event occurs and then automatically unsubscribed.
+            If set to `False` (default), the listener can be invoked multiple times until explicitly
+            unsubscribed.
+        :return: The event listener object associated with the subscribed callback function.
+        :raise PyeelibException: If the maximum number of listeners for an event has been exceeded.
+        """
+        # Retrieve all unique event keys
+        event_keys: Set[str] = set([cls._get_event_key(event=event) for event in events])
 
-                if not listeners:
-                    cls._event_listeners.pop(event)
+        # Acquire the lock to ensure exclusive access to the event registry
+        with cls._thread_lock:
+
+            # Check if the maximum number of listeners property is set
+            if cls._max_listeners is not None:
+
+                # For each event key, check if the maximum number of listeners for the event has been exceeded
+                for event_key in event_keys:
+                    if len(cls._event_registry.get(event_key, [])) > cls._max_listeners:
+                        raise PyeelibException(
+                            f"The event '{event_key}' has exceeded the maximum number of listeners allowed. "
+                            f"The '{callback.__name__}' listener cannot be added."
+                        )
+
+            # Create a new event listener
+            event_listener: EventListener = EventListener(callback=callback, once=once)
+
+            # For each event key, register the event listener
+            for event_key in event_keys:
+
+                # If the event key is not present in the event registry, create a new empty list for it
+                if event_key not in cls._event_registry:
+                    cls._event_registry[event_key] = []
+
+                # Append the event listener to the list of listeners for the event
+                cls._event_registry[event_key].append(event_listener)
+
+        # Return the new event listener
+        return event_listener
 
     @classmethod
-    def remove_event(cls, event: _EVENT_TYPES) -> bool:
-        key: str = cls._event_key(event=event)
+    def unsubscribe(cls, *events: SubscribableEventType, event_listener: EventListener) -> bool:
+        """
+        Unsubscribes an event listener from the specified events. The method removes the event
+        listener from the event registry and, if there are no more listeners for a particular
+        event, removes that event from the registry as well.
+        :param events: The events to unsubscribe from.
+        :param event_listener: The event listener to unsubscribe.
+        :return: `True` if the event listener associated with the events was found and removed, `False` otherwise.
+        """
+        # Retrieve all unique event keys
+        event_keys: Set[str] = set([cls._get_event_key(event=event) for event in events])
 
-        if key in cls._event_listeners:
-            cls._event_listeners.pop(key)
-            return True
+        # Flag indicating whether the event listener was successfully removed
+        deleted: bool = False
+
+        # Obtain the lock to ensure exclusive access to the event registry
+        with cls._thread_lock:
+
+            # For each event key, check and remove the event listener if found
+            for event_key in event_keys:
+                # Get the list of event listeners for the event key, or an empty list if it doesn't exist
+                event_listeners = cls._event_registry.get(event_key, [])
+
+                # Check if the event listener is present in the list of listeners for the event
+                if event_listener in event_listeners:
+                    # Remove the event listener from the list of listeners
+                    event_listeners.remove(event_listener)
+                    deleted = True
+
+                    # If there are no more listeners for the event, remove the event key from the registry
+                    if not event_listeners:
+                        cls._event_registry.pop(event_key)
+
+        # Return the flag indicating whether the event listener was deleted
+        return deleted
+
+    @classmethod
+    def remove_event_listener(cls, event_listener: EventListener) -> bool:
+        """
+        Removes an event listener from all subscribed events. If there are no more
+        listeners for a particular event, that event is removed from the registry.
+        :param event_listener: The event listener to remove.
+        :return: `True` if the event listener was found and removed, `False` otherwise.
+        """
+        # A flag indicating if the event listener gets removed
+        deleted: bool = False
+
+        # Acquire the lock to ensure exclusive access to the event registry
+        with cls._thread_lock:
+
+            # Iterate through each event and its associated listeners in the event registry
+            for event, event_listeners in cls._event_registry.items():
+
+                # Check if the event listener is present in the list of listeners for the event
+                if event_listener in event_listeners:
+                    # Remove the event listener from the list of listeners
+                    event_listeners.remove(event_listener)
+                    deleted = True
+
+                    # If there are no more listeners for the event, remove the event from the registry
+                    if not event_listeners:
+                        cls._event_registry.pop(event)
+
+        # Return the flag indicating if the event listener was found and deleted
+        return deleted
+
+    @classmethod
+    def remove_event(cls, event: SubscribableEventType) -> bool:
+        """
+        Removes an event and all associated event listeners.
+        :param event: The event to remove.
+        :return: `True` if the event was found and removed, `False` otherwise.
+        """
+        # Get the event key
+        event_key: str = cls._get_event_key(event=event)
+
+        # Acquire the lock to ensure exclusive access to the event registry
+        with cls._thread_lock:
+            # Check if the event key is present in the event registry
+            if event_key in cls._event_registry:
+                # Remove the event from the registry
+                cls._event_registry.pop(event_key)
+                return True
 
         return False
 
     @classmethod
-    def remove_all(cls) -> None:
-        cls._event_listeners = {}
+    def remove_all(cls) -> bool:
+        """
+        Removes all events and event listeners.
+        :return: `True` if the events were found and removed, `False` otherwise.
+        """
+        # Acquire the lock to ensure exclusive access to the event registry
+        with cls._thread_lock:
+            # Clear the event registry by assigning an empty dictionary
+            cls._event_registry = {}
+
+        return True

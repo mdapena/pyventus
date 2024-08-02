8865db32407e19eaef8de144e6c528028de0f916
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2024-08-02 01:08:53 -0400

    Add color formatting methods to StdoutColors

--- src/pyventus/core/constants/stdout_colors.py
+++ src/pyventus/core/constants/stdout_colors.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from typing import Callable
 
 
 class StdOutColors(ABC):
@@ -13,3 +14,13 @@ class StdOutColors(ABC):
     GREEN: str = "\033[32m"
     BOLD: str = "\033[1m"
     UNDERLINE: str = "\033[4m"
+
+    DEFAULT_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.DEFAULT}{text}"
+    PURPLE_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.PURPLE}{text}{StdOutColors.DEFAULT}"
+    YELLOW_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.YELLOW}{text}{StdOutColors.DEFAULT}"
+    BLUE_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.BLUE}{text}{StdOutColors.DEFAULT}"
+    RED_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.RED}{text}{StdOutColors.DEFAULT}"
+    CYAN_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.CYAN}{text}{StdOutColors.DEFAULT}"
+    GREEN_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.GREEN}{text}{StdOutColors.DEFAULT}"
+    BOLD_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.BOLD}{text}{StdOutColors.DEFAULT}"
+    UNDERLINE_TEXT: Callable[[str], str] = lambda text: f"{StdOutColors.UNDERLINE}{text}{StdOutColors.DEFAULT}"

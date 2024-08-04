8a1041d284634a3afa3108efa4facaab261d15d2
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2024-08-04 18:53:50 -0400

    Update EventRegistry API

--- src/pyventus/events/registry/event_registry.py
+++ src/pyventus/events/registry/event_registry.py
@@ -1,4 +1,3 @@
-from collections.abc import KeysView
 from typing import final, Dict, Set, Tuple
 
 from ..subscribers import EventSubscriber
@@ -7,12 +6,12 @@ from ..subscribers import EventSubscriber
 @final
 class EventRegistry:
     """
-    An optimized data structure for storing events and their event subscribers.
+    An optimized data structure for storing events and their subscribers.
 
     **Notes:**
 
-    -   This class employs a bidirectional mapping structure for efficient lookups, updates,
-        and deletions of events and their subscribers.
+    -   This class employs a multikeyed multivalued bidirectional mapping structure
+        for efficient lookups, updates, and deletions of events and their subscribers.
 
     -   Despite employing a bidirectional mapping structure, the memory footprint of this data
         structure remains minimal, thanks to the fact that events and their subscribers are
@@ -28,13 +27,13 @@ class EventRegistry:
     """
 
     # Attributes for the EventRegistry
-    __slots__ = ("__registry", "__inverse_registry")
+    __slots__ = ("__fwd_registry", "__inv_registry")
 
     def __init__(self) -> None:
         """Initializes an instance of `EventRegistry`."""
         # Initialize the registry and its inverse mapping
-        self.__registry: Dict[str, Set[EventSubscriber]] = {}
-        self.__inverse_registry: Dict[EventSubscriber, Set[str]] = {}
+        self.__fwd_registry: Dict[str, Set[EventSubscriber]] = {}
+        self.__inv_registry: Dict[EventSubscriber, Set[str]] = {}
 
     def get_registry(self) -> Dict[str, Set[EventSubscriber]]:
         """
@@ -42,57 +41,67 @@ class EventRegistry:
         :return: A mapping representing the main registry, where
             each event is mapped to a set of its subscribers.
         """
-        return self.__registry.copy()
+        return self.__fwd_registry.copy()
 
-    def get_events(self) -> KeysView[str]:
+    def get_events(self) -> Set[str]:
         """
         Retrieves all registered events.
         :return: A set of all registered events.
         """
-        return self.__registry.keys()
+        return set(self.__fwd_registry.keys())
 
-    def get_event_subscribers(self) -> KeysView[EventSubscriber]:
+    def get_subscribers(self) -> Set[EventSubscriber]:
         """
-        Retrieves all registered event subscribers.
-        :return: A set of all registered event subscribers.
+        Retrieves all registered subscribers.
+        :return: A set of all registered subscribers.
         """
-        return self.__inverse_registry.keys()
+        return set(self.__inv_registry.keys())
+
+    def get_events_from_subscribers(self, subscribers: Set[EventSubscriber]) -> Set[str]:
+        """
+        Retrieves a set of events associated with the specified subscribers.
+        :param subscribers: A set of subscribers for which to retrieve the associated events.
+        :return: A set of events associated with the given subscribers.
+        """
+        return {event for subscriber in subscribers for event in self.__inv_registry.get(subscriber, [])}
+
+    def get_subscribers_from_events(self, events: Set[str]) -> Set[EventSubscriber]:
+        """
+        Retrieves a set of subscribers associated with the specified events.
+        :param events: A set of events for which to retrieve the associated subscribers.
+        :return: A set of subscribers associated with the given events.
+        """
+        return {subscriber for event in events for subscriber in self.__fwd_registry.get(event, [])}
 
     def get_event_count(self) -> int:
         """
         Retrieves the number of events registered.
         :return: The total count of events currently registered.
         """
-        return len(self.__registry)
+        return len(self.__fwd_registry)
 
-    def get_event_subscriber_count(self) -> int:
+    def get_subscriber_count(self) -> int:
         """
-        Retrieves the number of event subscribers registered.
-        :return: The total count of event subscribers registered.
+        Retrieves the number of subscribers registered.
+        :return: The total count of subscribers registered.
         """
-        return len(self.__inverse_registry)
+        return len(self.__inv_registry)
 
-    def get_event_subscribers_by_event(self, event: str) -> Set[EventSubscriber] | None:
+    def get_event_count_from_subscriber(self, subscriber: EventSubscriber) -> int:
         """
-        Retrieves a set of event subscribers associated with the specified event.
-        :param event: The event to retrieve the event subscribers for.
-        :return: A set of event subscribers associated to the specified
-            event, or `None` if the event is not found.
+        Retrieves the count of events associated with the specified subscriber.
+        :param subscriber: The subscriber for which to count associated events.
+        :return: The number of events associated with the given subscriber.
         """
-        if event not in self.__registry:
-            return None
-        return set(self.__registry[event])
+        return len(self.__inv_registry[subscriber]) if subscriber in self.__inv_registry else 0
 
-    def get_events_by_event_subscriber(self, event_subscriber: EventSubscriber) -> Set[str] | None:
+    def get_subscriber_count_from_event(self, event: str) -> int:
         """
-        Retrieves a set of events to which the event subscriber is registered.
-        :param event_subscriber: The event subscriber to retrieve the events for.
-        :return: A set of event names that the event subscriber is registered to,
-            or `None` if the event subscriber is not found.
+        Retrieves the count of subscribers associated with the specified event.
+        :param event: The event for which to count associated subscribers.
+        :return: The number of subscribers associated with the given event.
         """
-        if event_subscriber not in self.__inverse_registry:
-            return None
-        return set(self.__inverse_registry[event_subscriber])
+        return len(self.__fwd_registry[event]) if event in self.__fwd_registry else 0
 
     def contains_event(self, event: str) -> bool:
         """
@@ -100,92 +109,92 @@ class EventRegistry:
         :param event: The event to be checked.
         :return: `True` if the event is registered, `False` otherwise.
         """
-        return event in self.__registry
+        return event in self.__fwd_registry
 
-    def contains_event_subscriber(self, event_subscriber: EventSubscriber) -> bool:
+    def contains_subscriber(self, subscriber: EventSubscriber) -> bool:
         """
-        Checks if the given event subscriber is registered.
-        :param event_subscriber: The event subscriber to be checked.
-        :return: `True` if the event subscriber is registered, `False` otherwise.
+        Checks if the given subscriber is registered.
+        :param subscriber: The subscriber to be checked.
+        :return: `True` if the subscriber is registered, `False` otherwise.
         """
-        return event_subscriber in self.__inverse_registry
+        return subscriber in self.__inv_registry
 
-    def insert(self, event: str, event_subscriber: EventSubscriber) -> None:
+    def insert(self, event: str, subscriber: EventSubscriber) -> None:
         """
-        Inserts the given event subscriber with the specified event in the registry.
-        :param event: The event to which the event subscriber will be associated.
-        :param event_subscriber: The event subscriber to be inserted for the event.
+        Inserts the given subscriber with the specified event in the registry.
+        :param event: The event to which the subscriber will be associated.
+        :param subscriber: The subscriber to be inserted for the event.
         :return: None
         """
-        # Add the event subscriber to the event's set in the main registry
-        if event not in self.__registry:
-            self.__registry[event] = set()
-        self.__registry[event].add(event_subscriber)
+        # Add the subscriber to the event's set in the main registry
+        if event not in self.__fwd_registry:
+            self.__fwd_registry[event] = set()
+        self.__fwd_registry[event].add(subscriber)
 
-        # Add the event to the event subscriber's set in the inverse registry
-        if event_subscriber not in self.__inverse_registry:
-            self.__inverse_registry[event_subscriber] = set()
-        self.__inverse_registry[event_subscriber].add(event)
+        # Add the event to the subscriber's set in the inverse registry
+        if subscriber not in self.__inv_registry:
+            self.__inv_registry[subscriber] = set()
+        self.__inv_registry[subscriber].add(event)
 
     def remove_event(self, event: str) -> Set[EventSubscriber] | None:
         """
-        Removes the specified event from the registry and returns a set of event subscribers
-        who were completely unsubscribed as a side effect of removing the event.
+        Removes the specified event from the registry and returns a set of subscribers
+        that were completely removed as a side effect of removing the event.
         :param event: The event to be removed from the registry.
-        :return: A set of event subscribers who were completely unsubscribed due to the
-            removal of the event, or `None` if the event is not found in the registry.
+        :return: A set of subscribers that were completely removed due to the
+            removal of the event, or `None` if the event is not found.
         """
         # If the event is not within the registry, return early
-        if event not in self.__registry:
+        if event not in self.__fwd_registry:
             return None
 
         # Remove the event and retrieve its subscribers
-        event_subscribers: Set[EventSubscriber] = self.__registry.pop(event)
+        subscribers: Set[EventSubscriber] = self.__fwd_registry.pop(event)
 
-        # Initialize a set to store the event subscribers
+        # Initialize a set to store the subscribers
         # that will be completely removed from both registries
-        removed_event_subscribers: Set[EventSubscriber] = set()
+        removed_subscribers: Set[EventSubscriber] = set()
 
-        # Remove the event from the event subscriber's set
-        for event_subscriber in event_subscribers:
-            self.__inverse_registry[event_subscriber].remove(event)
+        # Remove the event from the subscriber's set
+        for subscriber in subscribers:
+            self.__inv_registry[subscriber].remove(event)
 
-            # If the event subscriber was only subscribed to this event, remove it from
-            # the inverse registry and add it to the list of removed event subscribers
-            if len(self.__inverse_registry[event_subscriber]) == 0:
-                self.__inverse_registry.pop(event_subscriber)
-                removed_event_subscribers.add(event_subscriber)
+            # If the subscriber was only subscribed to this event, remove it from
+            # the inverse registry and add it to the list of removed subscribers
+            if len(self.__inv_registry[subscriber]) == 0:
+                self.__inv_registry.pop(subscriber)
+                removed_subscribers.add(subscriber)
 
-        # Return the removed event subscribers
-        return removed_event_subscribers
+        # Return the removed subscribers
+        return removed_subscribers
 
-    def remove_event_subscriber(self, event_subscriber: EventSubscriber) -> Set[str] | None:
+    def remove_subscriber(self, subscriber: EventSubscriber) -> Set[str] | None:
         """
-        Removes the specified event subscriber from the registry and returns a set of events
-        that were completely removed as a side effect of removing the event subscriber.
-        :param event_subscriber: The event subscriber to be removed from the registry.
+        Removes the specified subscriber from the registry and returns a set of events
+        that were completely removed as a side effect of removing the subscriber.
+        :param subscriber: The subscriber to be removed from the registry.
         :return: A set of events that were completely removed as a side effect of removing
-            the event subscriber, or `None` if the event subscriber is not found in the registry.
+            the subscriber, or `None` if the subscriber is not found.
         """
-        # If the event subscriber is not in the inverse registry, return early
-        if event_subscriber not in self.__inverse_registry:
+        # If the subscriber is not in the inverse registry, return early
+        if subscriber not in self.__inv_registry:
             return None
 
-        # Retrieve events associated with the event subscriber and remove it from the inverse registry
-        events: Set[str] = self.__inverse_registry.pop(event_subscriber)
+        # Retrieve events associated with the subscriber and remove it from the inverse registry
+        events: Set[str] = self.__inv_registry.pop(subscriber)
 
         # Initialize a set to store the events that will
         # be completely removed from both registries
         removed_events: Set[str] = set()
 
-        # Remove the event subscriber from the event's set
+        # Remove the subscriber from the event's set
         for event in events:
-            self.__registry[event].remove(event_subscriber)
+            self.__fwd_registry[event].remove(subscriber)
 
             # If the event had only that subscriber, remove it from
             # the registry and add it to the list of removed events
-            if len(self.__registry[event]) == 0:
-                self.__registry.pop(event)
+            if len(self.__fwd_registry[event]) == 0:
+                self.__fwd_registry.pop(event)
                 removed_events.add(event)
 
         # Return the removed events
@@ -193,18 +202,18 @@ class EventRegistry:
 
     def clear(self) -> Tuple[Set[str], Set[EventSubscriber]]:
         """
-        Clears the event registry by removing all events and event subscribers.
+        Clears the event registry by removing all events and subscribers.
         :return: A tuple containing two sets; the first set includes the events that were removed
-            from the registry, and the second set consists of the event subscribers that were removed.
+            from the registry, and the second set consists of the subscribers that were removed.
         """
-        # Store the current events and event subscribers before clearing the registry
-        events, event_subscribers = set(self.get_events()), set(self.get_event_subscribers())
+        # Store the current events and subscribers before clearing the registry
+        events, subscribers = self.get_events(), self.get_subscribers()
 
         # Clear both the main registry and the inverse registry
-        self.__registry.clear()
-        self.__inverse_registry.clear()
+        self.__fwd_registry.clear()
+        self.__inv_registry.clear()
 
-        return events, event_subscribers
+        return events, subscribers
 
     def __str__(self) -> str:
         """
@@ -214,8 +223,8 @@ class EventRegistry:
         return (
             "Event Registry: {\n"
             + "\n".join(
-                f"\t'{event}': {{{', '.join(f'EventSubscriber <{hex(id(sub))}>' for sub in event_subscribers)}}}"
-                for event, event_subscribers in self.__registry.items()
+                f"\t'{event}': {{{', '.join(f'EventSubscriber <{hex(id(sub))}>' for sub in subscribers)}}}"
+                for event, subscribers in self.__fwd_registry.items()
             )
             + "\n}"
         )

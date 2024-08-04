02273883fbe0d0dcfba3bdafd1356847ac650a2c
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2024-08-04 19:17:26 -0400

    Update EventSubscriber to reflect changes made in Subscription

--- src/pyventus/events/subscribers/event_subscriber.py
+++ src/pyventus/events/subscribers/event_subscriber.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Any, Awaitable, Callable, TypeAlias, Dict, final
+from typing import Any, Awaitable, Callable, TypeAlias, final
 
 from pyventus.core.exceptions import PyventusException
 from pyventus.core.subscriptions import Subscription
@@ -55,7 +55,7 @@ class EventSubscriber(EventHandler, Subscription):
 
     def __init__(
         self,
-        teardown_callback: Callable[[Self], None],
+        teardown_callback: Callable[[Self], bool],
         event_callback: EventCallbackType,
         success_callback: SuccessCallbackType | None,
         failure_callback: FailureCallbackType | None,
@@ -64,8 +64,10 @@ class EventSubscriber(EventHandler, Subscription):
     ) -> None:
         """
         Initializes an instance of `EventSubscriber`.
-        :param teardown_callback: A callback function that is invoked during unsubscription. This callback
-            is responsible for performing cleanup or teardown logic associated with the subscription.
+        :param teardown_callback: A callback function invoked during the unsubscription process to perform
+            cleanup or teardown operations associated with the subscription. It should return `True` if the
+            cleanup was successful, or `False` if the teardown has already been executed and the subscription
+            is no longer active.
         :param event_callback: The callback to be executed when the event occurs.
         :param success_callback: The callback to be executed when the event response completes successfully.
         :param failure_callback: The callback to be executed when the event response fails.
@@ -130,24 +132,6 @@ class EventSubscriber(EventHandler, Subscription):
         else:
             # Invoke the failure callback with the provided exception
             await self.__failure_callback(exception)
-
-    def __getstate__(self) -> Dict[str, Any]:
-        # Retrieve the state of the base Subscription class
-        state: Dict[str, Any] = super().__getstate__()
-
-        # Add the state of the EventSubscriber attributes
-        state.update(**{attr: self.__getattribute__(attr) for attr in EventSubscriber.__slots__})
-
-        # Return the complete state for serialization
-        return state
-
-    def __setstate__(self, state: Dict[str, Any]) -> None:
-        # Restore the state of the base Subscription class
-        super().__setstate__(state)
-
-        # Restore the state of the EventSubscriber attributes
-        for attr in EventSubscriber.__slots__:
-            self.__setattr__(attr, state[attr])
 
     def __str__(self) -> str:
         return (

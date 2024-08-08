94453cee86d968072d4ecf9f12dafc092b63fbbd
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2024-08-08 13:02:01 -0400

    Add Observer base class

---
+++ src/pyventus/reactive/observers/observer.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
+
+_in_T = TypeVar("_in_T", contravariant=True)
+"""A generic type representing the input value for the `next` method."""
+
+
+class Observer(ABC, Generic[_in_T]):
+    """
+    An abstract base class that defines the workflow and essential protocols for
+    responding to state changes emitted by an observable, including procedures
+    for handling `next`, `error`, and `complete` notifications.
+
+    **Notes:**
+
+    -   This class is parameterized by the type of value that will be received
+        in the `next` method.
+    """
+
+    # Allow subclasses to define __slots__
+    __slots__ = ()
+
+    @abstractmethod
+    async def next(self, value: _in_T) -> None:
+        """
+        Handle the next value emitted by the observable.
+        :param value: The value emitted by the observable.
+        :return: None
+        """
+        pass
+
+    @abstractmethod
+    async def error(self, exception: Exception) -> None:
+        """
+        Handle an error that occurs in the observable.
+        :param exception: The exception that was raised by the observable.
+        :return: None
+        """
+        pass
+
+    @abstractmethod
+    async def complete(self) -> None:
+        """
+        Handle the completion of the observable.
+        :return: None
+        """
+        pass

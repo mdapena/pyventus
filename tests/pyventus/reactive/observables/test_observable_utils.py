24367effe15eda875c0ff3ed8c4934dad8fcce63
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2024-09-28 22:27:37 -0400

    Rename TestAsObservable to TestObservableUtils and update tests

---
+++ tests/pyventus/reactive/observables/test_observable_utils.py
@@ -0,0 +1,38 @@
+import pytest
+from pyventus.reactive import ObservableTask, as_observable_task
+from typing_extensions import Any
+
+from ....fixtures import CallableMock
+from ....utils import get_private_attr
+
+
+class TestObservableUtils:
+    # =================================
+    # Test Cases for as_observable_task()
+    # =================================
+
+    @pytest.mark.parametrize(
+        ["callback", "args", "kwargs"],
+        [
+            (CallableMock.Sync(), (), {}),
+            (CallableMock.SyncGenerator(), ("pi", 3.14), {}),
+            (CallableMock.Async(), (), {"...": ...}),
+            (CallableMock.AsyncGenerator(), ("pi", 3.14), {"...": ...}),
+        ],
+    )
+    def test_as_observable_task_decorator(
+        self, callback: CallableMock.Base, args: tuple[Any, ...], kwargs: dict[str, Any]
+    ) -> None:
+        # Arrange
+        decorated_callback = as_observable_task(callback)
+
+        # Act
+        observable_task: ObservableTask[Any] = decorated_callback(*args, **kwargs)
+        observable_task_callback = get_private_attr(observable_task, "__callback")
+
+        # Assert
+        assert callable(decorated_callback)
+        assert isinstance(observable_task, ObservableTask)
+        assert get_private_attr(observable_task_callback, "__callable") is callback
+        assert get_private_attr(observable_task, "__args") == args
+        assert get_private_attr(observable_task, "__kwargs") == kwargs

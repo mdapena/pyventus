29f40069aec75e2266f75ea3984cb3dee2e231a0
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2025-05-18 13:10:20 +0200

    Update logger tests

--- tests/pyventus/core/loggers/test_logger.py
+++ tests/pyventus/core/loggers/test_logger.py
@@ -1,7 +1,8 @@
+from logging import DEBUG
 from typing import Any
 
 import pytest
-from pyventus.core.loggers import Logger
+from pyventus.core.loggers import Logger, StdOutLogger
 
 
 class TestLogger:
@@ -20,11 +21,13 @@ class TestLogger:
     )
     def test_creation(self, source: Any) -> None:
         # Arrange | Act
+        StdOutLogger.config(level=DEBUG)
         logger = Logger(source=source, debug=True)
 
         # Assert
         assert logger and logger.debug_enabled
-        logger.info("info")
-        logger.debug("debug")
+        logger.critical("critical")
         logger.error("error")
         logger.warning("warning")
+        logger.info("info")
+        logger.debug("debug")

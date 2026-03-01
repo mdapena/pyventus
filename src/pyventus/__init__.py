45e4810205a30ea8d986c610c1d748c46be06799
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2026-03-01 16:53:00 +0100

    Release version 0.8.0

--- src/pyventus/__init__.py
+++ src/pyventus/__init__.py
@@ -1,6 +1,6 @@
 """A Python library for event-driven and reactive programming."""
 
-__version__ = "0.7.2"
+__version__ = "0.8.0"
 
 from .core.exceptions import PyventusException, PyventusImportException
 
